42d3b18410e4e51aacced9f0a21f37f71bd14cbb
manuelpc97 <manuelpc97@unitec.edu>
2016-01-29 16:14:51 -0600

    Ejercicios terminados

--- Lab2Progra3.cpp
+++ Lab2Progra3.cpp
@@ -7,11 +7,14 @@ using std::endl;
 
 unsigned long long int factorial(int numero);
 double euler(int numero);
-
+double ladosTrapezio(int a1, int a2, int b1, int b2);
+double altura(double lado1, double lado2, double lado3, double semi);
+double area(double alt, double diagonal);
 int main(int argc, char*argv[]){
-	int opcion;
-	int numero;
-	double resp;
+	int opcion, numero;
+	double a1,a2,b1,b2,c1,c2,d1,d2;
+	double resp, lado1=0.0,lado2=0.0,lado3=0.0,lado4=0.0, diagonal=0.0, semi1=0.0, semi2=0.0;
+	double alt1, alt2,alt3,alt4,alt5,alt6;
 
 	cout<<"Ingrese la operacion que desea realizar"<<endl;
 	cout<<"1.- Ejercicio 1"<<endl;
@@ -24,10 +27,87 @@ int main(int argc, char*argv[]){
 		resp = euler(numero);
 		cout<<"El numero seria: "<<resp<<endl;
 	}else if(opcion==2){
-		
-	}else{
+		cout<<"Ingrese las coordenadas de su primer punto"<<endl;
+		cin>>a1;
+		cin>>a2;
 
-	}	
+		cout<<"Ingrese las coordenadas de su segundo punto"<<endl;
+                cin>>b1;
+                cin>>b2;
+
+		cout<<"Ingrese las coordenadas de su tercer punto"<<endl;
+                cin>>c1;
+                cin>>c2;
+
+		cout<<"Ingrese las coordenadas de su cuarto punto"<<endl;
+                cin>>d1;
+                cin>>d2;
+
+		
+		
+		
+		if((a1==b1&&a1==c1)||(a1==c1&&a1==d1)||(b1==c1&&b1==d1)){
+			cout<<"Tres numeros forman una linea, coordenadas incorrectas"<<endl;
+		}else if((a2==b2&&a2==c2)||(a2==c2&&a2==d2)||(b2==c2&&b2==d2)){
+			cout<<"Tres numeros forman una linea, coordenadas incorrectas"<<endl;
+		}else{
+
+		cout<<"Los puntos son: "<<endl;
+		cout<<"("<<a1<<","<<a2<<")"<<endl;
+		cout<<"("<<b1<<","<<b2<<")"<<endl;
+		cout<<"("<<c1<<","<<c2<<")"<<endl;
+		cout<<"("<<d1<<","<<d2<<")"<<endl;
+		
+		lado1=ladosTrapezio(a1,a2,b1,b2);
+		lado2=ladosTrapezio(b1,b2,d1,d2);
+		lado3=ladosTrapezio(a1,a2,c1,c2);
+		lado4=ladosTrapezio(c1,c2,d1,d2);
+		diagonal = ladosTrapezio(a1,b2,d1,d2);
+
+		cout<<"Los lados del trapezoide miden: "<<endl;
+		cout<<"Lado 1: "<<lado1<<endl;
+		cout<<"Lado 2: "<<lado2<<endl;
+		cout<<"Lado 3: "<<lado3<<endl;
+		cout<<"Lado 4: "<<lado4<<endl;
+
+		cout<<"LOs lados del triangulo 1 miden = "<<endl;
+		cout<<"Lado 1: "<<lado1<<endl;
+		cout<<"Lado2: "<<lado2<<endl;
+		cout<<"Lado3: "<<diagonal<<endl;
+
+		cout<<"LOs lados del triangulo 2 miden = "<<endl;
+                cout<<"Lado 1: "<<lado3<<endl;
+                cout<<"Lado2: "<<lado4<<endl;
+                cout<<"Lado3: "<<diagonal<<endl;
+		
+		semi1=(lado1 +lado2+diagonal)/2;
+		semi2=(lado3+lado4+diagonal)/2;
+		cout<<"El semiperimetro del triangulo 1  es: "<<semi1<<endl;
+		cout<<"El semiperimetro del triangulo 2  es: "<<semi2<<endl;
+		cout<<"El perimetro del triangulo 1  es: "<<lado1+lado2+diagonal<<endl;
+		cout<<"El perimetro del triangulo 2  es: "<<lado3+lado4+diagonal<<endl;
+
+		alt1=altura(lado1, lado2, diagonal, semi1);
+		alt2=altura(lado2, lado1, diagonal, semi1);
+		alt3=altura(diagonal, lado2, lado1, semi1);
+		alt4=altura(lado3, lado4, diagonal, semi2);
+		alt5=altura(lado4, lado3, diagonal, semi2);
+		alt6=altura(diagonal, lado4, lado3, semi2);
+
+		cout<<"Las alturas del traingulo 1 son: "<<endl;
+		cout<<"Altura 1: "<<alt1<<endl;
+		cout<<"Altura 2: "<<alt2<<endl;
+		cout<<"Altura 3: "<<alt3<<endl;
+
+		cout<<"Las alturas del traingulo 2 son: "<<endl;
+                cout<<"Altura 1: "<<alt4<<endl; 
+                cout<<"Altura 2: "<<alt5<<endl;
+                cout<<"Altura 3: "<<alt6<<endl;			
+
+		cout<<"El area del trapezio es: "<<area(alt3, diagonal)+area(alt6,diagonal)<<endl;
+}				
+}
+return 0;	
 }
 
 unsigned long long int factorial(int numero){
@@ -47,5 +127,23 @@ double euler(int numero){
 	for(int i = 1; i<=limite;i++){
 		respuesta+=(pow(numero,i))/(factorial(i));
 	}
-return respuesta;	
+	return respuesta;	
+}
+
+double ladosTrapezio(int a1, int a2, int b1, int b2){
+	double medida=0;
+	medida = sqrt(pow(a1-a2,2)+pow(b1-b2,2));
+	return medida;
+}
+
+double altura(double lado1, double lado2, double lado3, double semi){
+	double altura=0.0;
+
+	altura=(2/lado1)*(sqrt(semi*(semi-lado1)*(semi-lado2)*(semi-lado3)));
+	return altura;
+}
+
+double area(double alt, double diagonal){
+	double ar;
+	ar= (diagonal*alt)/2;
 }
