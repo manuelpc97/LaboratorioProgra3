823afffe5a059e720c7055717729e802ab90f78a
manuelpc97 <manuelpc97@unitec.edu>
2016-01-29 14:11:22 -0600

    Primer ejercicio con limite 15

---
+++ Lab2Progra3.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int factorial(int numero);
+double euler(int numero);
+
+int main(int argc, char*argv[]){
+	int opcion;
+	int numero;
+	double resp;
+
+	cout<<"Ingrese la operacion que desea realizar"<<endl;
+	cout<<"1.- Ejercicio 1"<<endl;
+	cout<<"2. Ejercicio 2"<<endl;
+	cin>>opcion;
+
+	if(opcion==1){
+		cout<<"Ingrese un numero para calcular el numero de Euler"<<endl;
+		cin>>numero;
+		resp = euler(numero);
+		cout<<"El numero seria: "<<resp<<endl;
+	}else if(opcion==2){
+
+	}else{
+
+	}	
+}
+
+int factorial(int numero){
+	int resultado=1;
+
+	for(int i = 1; i<=numero;i++){
+		resultado*=i;
+	}
+
+	return resultado;
+}
+
+double euler(int numero){
+	int limite = 15;
+	double respuesta=1.0;
+
+	for(int i = 1; i<=limite;i++){
+		respuesta+=(pow(numero,i))/(factorial(i));
+	}
+return respuesta;	
+}
